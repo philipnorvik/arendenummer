c56aa7dd05b4c55c60562ebb6ecb2df0ca9a1031
PhilipN <01.philip@gmail.com>
2020-04-05 19:31:16 +0200

    Added security

--- src/main/java/se/rsv/arende/arendeinformationspring/controller/MainController.java
+++ src/main/java/se/rsv/arende/arendeinformationspring/controller/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import se.rsv.arende.arendeinformationspring.model.Arende;
@@ -15,10 +16,23 @@ import se.rsv.arende.arendeinformationspring.service.ArendeInformationService;
 
 
 @Controller
-public class RegisteringController {
+public class MainController {
 	
 	@Autowired
 	ArendeInformationService service;
+	
+	 // Login form
+	  @RequestMapping("/login.html")
+	  public String login() {
+	    return "register";
+	  }
+
+	  // Login form with error
+	  @RequestMapping("/login-error.html")
+	  public String loginError(Model model) {
+	    model.addAttribute("loginError", true);
+	    return "login.html";
+	  }
 	
 	@GetMapping("/register")
 	public String registerForm (Model model) {
